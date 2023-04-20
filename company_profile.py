285572b104ee840b30239819ac9b4060d13b4066
Andrew Maher <amaher423@gmail.com>
2023-04-20 17:12:39 -0500

    Added company_profile model

---
+++ company_profile.py
@@ -0,0 +1,13 @@
+class CompanyProfile:
+
+    name = None
+    marketCap = None
+    employees = None
+    dividendYield = None
+    nextDividendDate = None
+    nextEarningsDate = None
+    peRatio = None
+    
+
+    def function(self):
+        print("This is a message inside the class")
